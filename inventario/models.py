11d42ceacba3f93b1a172a21f8cf49e9d8edf5c0
Davi Castro <davialmeida47jd@gmail.com>
2023-05-18 18:22:58 -0300

    feat(models): created products model and generated migrate for database

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    ean = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255)
+    supplier = models.CharField(max_length=255, null=True)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.DecimalField(max_digits=10, decimal_places=3, default=0.000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
